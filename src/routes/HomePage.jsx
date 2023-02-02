82019162e79b2ec153fac70f5945477b729852fd
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-02-02 15:11:00 +0100

    initialized a new website project, installed all packages that will be used and created the AboutUs section

---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+const HomePage = () => {
+    return (
+        <>
+            <AboutSection />
+        </>
+    )
+}
+
+export default HomePage
