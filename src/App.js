9b7b3c60dbd11460bd925957ce577b0111e78028
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-02-03 04:55:01 +0100

    styled the about us section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import HomePage from "./routes/HomePage";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+    <GlobalStyle />
       <HomePage />
     </div>
   );
