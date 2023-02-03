407cb69996cfd08abea5b14df16681354eb4c798
Faisal-pixel <adamsfaisal2003@gmail.com>
2023-02-03 04:27:31 +0100

    ServicesSection and FaqSection

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import FaqSection from "../components/FaqSection";
+import ServicesSection from "../components/ServicesSection";
 
 const HomePage = () => {
     return (
         <>
             <AboutSection />
+            <ServicesSection />
+            <FaqSection />
         </>
     )
 }
